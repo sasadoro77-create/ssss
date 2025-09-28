69846b6f4f64a5fdd48219dd4506e510e0c9b095
sasadoro77-create <sasalisicka77@gmail.com>
2025-09-28 15:59:32 +0300

    Add files via upload

---
+++ omg.php
@@ -0,0 +1,8 @@
+<?php
+$password = "111";
+$password2 = readline("Введите пароль:");
+if ($password === $password2) {
+    echo "true";
+} else {
+    echo "false";
+}
---
+++ omg2.php
@@ -0,0 +1,9 @@
+<?php
+$o = readline("Введите числа1:");
+$m = readline("Введите числа2:");
+$g = readline("Введите числа3:");
+if ($m - $o === $s - $m) {
+    echo "true";
+} else {
+    echo "false";
+}
